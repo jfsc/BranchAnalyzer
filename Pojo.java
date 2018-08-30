24bb362ea8168856e5c6d9a5b0ba97ef5b39fa70
Fish <jfsc@github.com>
2018-08-29 22:12:24 -0300

    Pojo for test

---
+++ Pojo.java
@@ -0,0 +1,36 @@
+package com.fish.branchanalyzer.tests.helper.data;
+
+public class Pojo2 {
+
+    private long idPedido;
+
+    private long idCliente;
+
+    private String item2;
+
+    public long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public long getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(long idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem(String item2) {
+        novo
+	this.item2 = item2;
+    }
+}
+
